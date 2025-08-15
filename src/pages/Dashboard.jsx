525bd3474948c76c9e88a344957d17916b63d3ce
Jerwin Titus <titusjerwin@gmail.com>
2025-08-15 14:33:54 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 import AddHabitForm from "../components/AddHabitForm";
 import HabitList from "../components/HabitList";
 import MonsterStatus from "../components/MonsterStatus";
